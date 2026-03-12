4a313f11b0b442ae9dc999072ce8f681d9d3e01c
kamrulhasan2 <kamrulhasan20656@gmail.com>
2026-03-12 17:02:10 +0600

    fix(cors): fixed cors related issue and update frontend url

--- src/shared/middlewares/applyMiddleware.ts
+++ src/shared/middlewares/applyMiddleware.ts
@@ -1,25 +1,33 @@
 import cors from 'cors';
 import morgan from 'morgan';
-import { logger } from '../utils'
+import { logger } from '../utils';
 import * as rTracer from 'cls-rtracer';
 import favicon from 'serve-favicon';
 import path from 'path';
+import { configEnv } from '../../config/env.config';
 
 const applyMiddleware = (app: any) => {
-    app.use(cors());
+  app.use(
+    cors({
+      origin: configEnv.focustube_frontend_url, // Allow Next.js frontend
+      credentials: true, // Allow cookies/auth headers
+    }),
+  );
 
-    // Serve favicon
-    app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')));
+  // Serve favicon
+  app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')));
 
-    // 1. Assign a Request ID to every incoming request
-    app.use(rTracer.expressMiddleware());
+  // 1. Assign a Request ID to every incoming request
+  app.use(rTracer.expressMiddleware());
 
-    // Bridge Morgan (HTTP) to Winston
-    app.use(morgan(':method :url :status - :response-time ms', {
-        stream: {
-            write: (message) => logger.http(message.trim())
-        }
-    }));
-}
+  // Bridge Morgan (HTTP) to Winston
+  app.use(
+    morgan(':method :url :status - :response-time ms', {
+      stream: {
+        write: message => logger.http(message.trim()),
+      },
+    }),
+  );
+};
 
 export default applyMiddleware;
